ea477d013000a181fbadebb5ce48fc2693989daa
Tracy Royal <TracyRoyal@mac.com>
2016-05-17 12:09:59 -0400

    a few changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # resource :welcome, only: [] do
+  #   collection do
+  #     get :about
+  #     get :index
+  #   end
+  # end
+  #
+  # resources :posts, only: :index
+
   get "welcome/index"
   get "welcome/about"
 
